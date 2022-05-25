41567343a3c9edb6a3c3fb1ea4dfcca81488b0b8
ymorimot <dorariver123@gmail.com>
2022-05-25 21:24:58 +0900

    imac525

--- libft.h
+++ libft.h
@@ -6,13 +6,12 @@
 /*   By: ymorimot <ymorimot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 23:11:22 by ymorimot          #+#    #+#             */
-/*   Updated: 2022/05/07 17:10:50 by ymorimot         ###   ########.fr       */
+/*   Updated: 2022/05/25 05:24:18 by ymorimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-
 # include <stddef.h>
 
 int		ft_toupper(int c);
@@ -20,6 +19,11 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char	*s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void	*memchr(const void *s, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+size_t	ft_strlen(const char *str);
+int		ft_atoi(const char *str);
+int		ft_isdigit(int c);
 
 #endif
